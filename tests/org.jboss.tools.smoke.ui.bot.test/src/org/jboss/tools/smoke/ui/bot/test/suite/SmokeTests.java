ef8b4683352461215335e440905b43ef4512eaac
Andrej Podhradsky <apodhrad@redhat.com>
2015-02-25 14:06:42 +0100

    Smoke bundle

---
+++ tests/org.jboss.tools.smoke.ui.bot.test/src/org/jboss/tools/smoke/ui/bot/test/suite/SmokeTests.java
@@ -0,0 +1,22 @@
+package org.jboss.tools.smoke.ui.bot.test.suite;
+
+import junit.framework.TestSuite;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.switchyard.ui.bot.test.HelloWorldTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Test Suite
+ * 
+ * @author apodhrad
+ *
+ */
+@SuiteClasses({
+	HelloWorldTest.class
+})
+@RunWith(RedDeerSuite.class)
+public class SmokeTests extends TestSuite {
+
+}
